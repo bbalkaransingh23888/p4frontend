e2c96c83f3bfcca3f4769fe8c308c5194f5b9cbf
Marilyn Balkaransingh <marilynbalkaransingh@marilyns-air.home>
2020-09-18 13:36:49 -0400

    just checking

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,10 +3,10 @@
     <b-navbar>
       <template slot="brand">
         <b-navbar-item tag="router-link" :to="{ path: '/' }">
-          <img
+          <!--img
             src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
             alt="Lightweight UI components for Vue.js based on Bulma"
-          />
+          /-->
         </b-navbar-item>
       </template>
       <template slot="start">
