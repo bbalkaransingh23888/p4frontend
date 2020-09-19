230d66e7fde534bdc976c8b9cdd7939022bbc4ad
Marilyn Balkaransingh <marilynbalkaransingh@marilyns-air.home>
2020-09-19 17:42:10 -0400

    CRUD functions that are commented out because there's something wrong and I have no idea what it is yet

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="main">
-     <b-field label="Category Name" v-model="name"
-            type="is-danger"
-            message="Create a category">
-            <b-input type="text" maxlength="30">
-            </b-input>
+        
+        <!--Create Category -->
+        <b-field label="Category" type="is-danger" message="Create a category">
+            <b-input type="text" v-model="category" maxlength="30"> </b-input>
         </b-field>
         <b-button type="is-danger" @click="newCategory">Create Category</b-button><br/><br/>
+        
+        <!--Edit Category -->
+        <b-field label="Category" type="is-danger" message="Edit category">
+            <b-input type="text" v-model="editcategory" maxlength="30"> </b-input>
+        </b-field>
+        <b-button type="is-danger" @click="editCategory" v-bind:id="editid">Edit</b-button><br/><br/>
+    
     <ul>
-      <li v-for="category of categories" v-bind:key="category.id">{{category.title}}</li>
+      <li v-for="category of categories" v-bind:key="category.id">
+        {{category.name}}
+        <b-button type="is-danger" v-bind:id="category.id" @click="deleteCategory">Delete</b-button>
+        <b-button type="is-danger" v-bind:id="category.id" @click="() => editSelect(category.id, category.name)">Edit</b-button>
+      </li>
     </ul>
   </div>
 </template>
@@ -20,40 +30,80 @@ export default {
   data: function(){
     return {
       categories: [],
-      games: []
+      editCategory: "",
+      editid: null
     }
   },
-  // created: function(){
-  //   this.getCategories();
-  //   },
-  //   methods: {
-  //   newCategory: function(){
-  //   const{ tokens, URL } = this.$route.query;
+  //Comment out here
+//   created: function(){
+//     this.getCategories();
+//     },
+//     methods: {
+// newCategory: function(){
+//     const{ tokens, URL } = this.$route.query;
 
-  //   fetch(`${URL}/api/categories`, {
-  //     method: "post",
-  //     headers: {
-  //       authorization: `jwt ${tokens.access}`,
-  //       "Content-Type": "application/json" 
-  //     },
-  //     body: JSON.stringify({ category: this.category }),
-  //     }).then(() => {
-  //       this.getCategories();
-  //     });
-  //   },
-  //     getCategories: function(){
-  //     const { tokens, URL } = this.$route.query;
+//     fetch(`${URL}/api/categories`, {
+//       method: "post",
+//       headers: {
+//         authorization: `jwt ${tokens.access}`,
+//         "Content-Type": "application/json" 
+//       },
+//       body: JSON.stringify({ category: this.category }),
+//       }).then(() => {
+//         this.getCategories();
+//       });
+//     },
+// getCategories: function(){
+//       const { tokens, URL } = this.$route.query;
 
-  //     fetch(`${URL}/api/categories/`, {
-  //       method: "get",
-  //       headers: {
-  //         authorization: `jwt ${tokens.access}`,
-  //       },
-  //       body: JSON.stringify({category: this.category}),
-  //     }).then((response) => {
-  //       this.getCategories();
-  //     });
-  //   }
-  // }
+//       fetch(`${URL}/api/categories/`, {
+//         method: "get",
+//         headers: {
+//           authorization: `jwt ${tokens.access}`,
+//         },
+//           body: JSON.stringify({category: this.category}),
+//         })
+//         .then((response) => response.json())
+//         .then((data) => {
+//           this.categories = data;
+//         });
+//       },
+//     deleteCategory: function(){
+//         const{ tokens, URL } = this.$route.query;
+//         const id = event.target.id
+
+//         fetch(`${URL}/api/categories/${id}/`, {
+//           method: "delete",
+//           headers: {
+//             authorization: `jwt ${tokens.access}`,
+//           },
+//         })
+//         .then(() => {
+//           this.getCategories()
+//         });
+//       },
+//       editSelect: function(id, content){
+//         this.editid = id
+//         this.editCategory = content
+//       },
+//       editCategory: function(){
+//         const{ tokens, URL } = this.$route.query;
+//         const id = this.editCategory
+
+//         fetch(`${URL}/api/categories/${id}/`, {
+//           method: "put",
+//           headers: {
+//             authorization: `jwt ${tokens.access}`,
+//             "Content-Type": "application/json"
+//           },
+//           body: JSON.stringify({category: this.editCategory})
+//         })
+//     },
+//   }
+  //Stop commenting out here
 }
 </script>
+
+<style>
+
+</style>
